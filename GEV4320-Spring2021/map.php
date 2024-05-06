18269e1ddcaedaeae949d627f9a758845d4187e2
Erica Y. Hayes <ericay.hayes@gmail.com>
2024-05-06 12:03:27 -0400

    updates

--- GEV4320-Spring2021/map.php
+++ GEV4320-Spring2021/map.php
@@ -1,3 +1,7 @@
+<?php if ((parse_url($_GET['path'], PHP_URL_HOST) == "arcg.is") ||
+	(parse_url($_GET['path'], PHP_URL_HOST) == "villanova.maps.arcgis.com") ||
+	(parse_url($_GET['path'], PHP_URL_HOST) == "www.arcgis.com") ||
+	(parse_url($_GET['path'], PHP_URL_HOST) == "storymaps.arcgis.com")) { ?>
 <html>
 	<head>
 		<meta charset="UTF-8">
@@ -7,6 +11,7 @@
 		<script src="scripts/app.js" type="text/javascript"></script>
 	</head>
 	</head>
+
 	<body>
 		<nav>
 			<p class="back-link">
@@ -15,11 +20,13 @@
 			<h1 id="map-title">GEV 4320/8320 Story Maps</h1>
 			<div id="scroll-icon"><img src="images/one_finger_scroll2.png" alt=""></div>
 		</nav>
-		<?php if ((parse_url($_GET['path'], PHP_URL_HOST) == "arcg.is") || 
-			  (parse_url($_GET['path'], PHP_URL_HOST) == "villanova.maps.arcgis.com") ||
-			  (parse_url($_GET['path'], PHP_URL_HOST) == "www.arcgis.com") ||
-			  (parse_url($_GET['path'], PHP_URL_HOST) == "storymaps.arcgis.com")) { ?>
 		<iframe id="map-frame" src="<?= $_GET['path']?>" width="100%" height="100%" frameborder="0"></iframe>
-		<?php } ?>
 	</body>
 </html>
+	
+<?php } else {
+    header($_SERVER["SERVER_PROTOCOL"]." 410 Gone"); 
+    $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'];
+    header("Refresh: 0; url=$url");
+    exit;
+} ?>
